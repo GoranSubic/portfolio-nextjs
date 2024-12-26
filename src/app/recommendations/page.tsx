b67918f2c41aeaf058e774f5038f950d0093747a
Goran Subic <goran.subic@ext.webedia-group.com>
2024-12-26 12:54:09 +0100

    Test vercel deployment - Test text.

--- src/app/recommendations/page.tsx
+++ src/app/recommendations/page.tsx
@@ -1,44 +1,31 @@
-import { RestliClient } from 'linkedin-api-client';
+// import { RestliClient } from 'linkedin-api-client';
 // import { NextResponse } from 'next/server';
 import { BaseLayout } from "@/layouts/BaseLayout";
-import Image from 'next/image';
-import { IRecommendation } from '@/types/recommendations';
+// import Image from 'next/image';
+// import { IRecommendation } from '@/types/recommendations';
 
-export const revalidate = 3600 // invalidate every hour
 
-export default async function Recommendations() {
-  const getRecommendations = async () => {
-    const accessToken: string | undefined = process.env.LINKEDIN_ACCESS_TOKEN;
-  
-    if (!accessToken) {
-        // return NextResponse.json({ error: 'Access token is required' }, { status: 500 });
-        throw new Error('Access token is required');
-    }
+export default async function Page() {
+  // const accessToken: string | undefined = process.env.LINKEDIN_ACCESS_TOKEN;
 
-    const restliClient = new RestliClient();
-    // restliClient.setDebugParams({ enabled: true });
+  // if (!accessToken) {
+  //     return NextResponse.json({ error: 'Access token is required' }, { status: 500 });
+  // }
 
-    const response = await restliClient.get({
-        resourcePath: '/userinfo',
-        accessToken
-    });
+  // const restliClient = new RestliClient();
+  // restliClient.setDebugParams({ enabled: true });
 
-    if (response.status !== 200) {
-      throw new Error('Failed to fetch LinkedIn recommendations');
-    }
+  // const response = await restliClient.get({
+  //     resourcePath: '/userinfo',
+  //     accessToken
+  // });
 
-    const recommendationsData = response.data;
-    const recommendations: IRecommendation = {
-      name: recommendationsData.name,
-      email: recommendationsData.email,
-      picture: recommendationsData.picture,
-    };
-
-    return recommendations;
-  }
-
-  const recommendations = await getRecommendations();
-  
+  // const recommendationsData = response.data;
+  // const recommendations: IRecommendation = {
+  //   name: recommendationsData.name,
+  //   email: recommendationsData.email,
+  //   picture: recommendationsData.picture,
+  // };
 
   // const LINKEDIN_API_URL = 'https://api.linkedin.com/v2/userinfo';
 
@@ -59,15 +46,7 @@ export default async function Recommendations() {
       <div>
           <h1>Recommendations</h1>
           <ul>
-              <li key={recommendations.name}>
-              <p>{recommendations.email}</p>
-              <Image
-                  src={recommendations.picture}
-                  alt={recommendations.name}
-                  width={100}
-                  height={100}
-              />
-              </li>
+            <li>Test</li>
           </ul>
       </div>
     </BaseLayout>
