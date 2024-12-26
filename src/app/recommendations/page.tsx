c68c0bd39ee76fe06e4a2fcf6d13205c81aef214
Goran Subic <goran.subic@ext.webedia-group.com>
2024-12-26 12:44:36 +0100

    Test vercel deployment - getRecommendations function.

--- src/app/recommendations/page.tsx
+++ src/app/recommendations/page.tsx
@@ -1,42 +1,44 @@
 import { RestliClient } from 'linkedin-api-client';
-import { NextResponse } from 'next/server';
+// import { NextResponse } from 'next/server';
 import { BaseLayout } from "@/layouts/BaseLayout";
 import Image from 'next/image';
 import { IRecommendation } from '@/types/recommendations';
 
 export const revalidate = 3600 // invalidate every hour
 
-export default async function Page() {
-  const accessToken: string | undefined = process.env.LINKEDIN_ACCESS_TOKEN;
+export default async function Recommendations() {
+  const getRecommendations = async () => {
+    const accessToken: string | undefined = process.env.LINKEDIN_ACCESS_TOKEN;
+  
+    if (!accessToken) {
+        // return NextResponse.json({ error: 'Access token is required' }, { status: 500 });
+        throw new Error('Access token is required');
+    }
 
-  if (!accessToken) {
-      return NextResponse.json({ error: 'Access token is required' }, { status: 500 });
+    const restliClient = new RestliClient();
+    // restliClient.setDebugParams({ enabled: true });
+
+    const response = await restliClient.get({
+        resourcePath: '/userinfo',
+        accessToken
+    });
+
+    if (response.status !== 200) {
+      throw new Error('Failed to fetch LinkedIn recommendations');
+    }
+
+    const recommendationsData = response.data;
+    const recommendations: IRecommendation = {
+      name: recommendationsData.name,
+      email: recommendationsData.email,
+      picture: recommendationsData.picture,
+    };
+
+    return recommendations;
   }
 
-  let recommendations: IRecommendation = {
-    name: '',
-    email: '',
-    picture: '',
-  };
-
-  const restliClient = new RestliClient();
-  // restliClient.setDebugParams({ enabled: true });
-
-  const response = await restliClient.get({
-      resourcePath: '/userinfo',
-      accessToken
-  });
-
-  if (response.status !== 200) {
-    throw new Error('Failed to fetch LinkedIn recommendations');
-  }
-
-  const recommendationsData = response.data;
-  recommendations = {
-    name: recommendationsData.name,
-    email: recommendationsData.email,
-    picture: recommendationsData.picture,
-  };
+  const recommendations = await getRecommendations();
+  
 
   // const LINKEDIN_API_URL = 'https://api.linkedin.com/v2/userinfo';
 
