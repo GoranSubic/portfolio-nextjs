4f9ec8304a96669b3fb840fffcfd0615f578af33
Goran Subic <goran.subic@ext.webedia-group.com>
2024-12-31 21:26:08 +0100

    Vercel error corrections -  missing file!

---
+++ src/types/componentDetails.ts
@@ -0,0 +1,9 @@
+import { IconType } from "react-icons";
+
+export interface IComponentDetails {
+  icon: IconType;
+  title: string;
+  description: string;
+  techStack: string;
+  componentURL: string;
+}
