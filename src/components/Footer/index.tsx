95a72e75e14c4046c5aaf59afaeaed977d90c3d6
Goran Subic <goran.subic@ext.webedia-group.com>
2025-01-05 14:07:14 +0100

    Footer corrections.

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -6,33 +6,34 @@ export const Footer: FC = () => {
   return (
     <div className={classes.root}>
       <div className="flex flex-row">
-        <p className={classes.builtByText}>
-          Built with&nbsp;
-          {LIBRARIES_USED_LINKS.map((externalLink) => (
-            <a
-              key={externalLink.link}
-              href={externalLink.link}
-              target={"_blank"}
-              rel={"noreferrer nofollow"}
-            >
-              <span>
-                {<externalLink.component aria-label={externalLink.name} />}
-              </span>
-            </a>
-          ))}
-        </p>
-      </div>
-      <div className={classes.bottomFooter}>
-        <div className={classes.myText}>
-          <p>Goran Subić</p>
-          <p><a href="Tel:+381658583432">Phone: +381 65/85-83-432</a></p>
-          <p><a href="Mailto:gsubic@gmail.com">Email: gsubic@gmail.com</a></p>
+        <div className={classes.bottomFooter}>
+          <div className={classes.myText}>
+            <p>Goran Subić</p>
+            <p><a href="Tel:+381658583432">Phone: +381 65/85-83-432</a></p>
+            <p><a href="Mailto:gsubic@gmail.com">Email: gsubic@gmail.com</a></p>
+          </div>
+          <p className={classes.builtByText}>
+            <span>Built using:</span>
+            <span>
+              {LIBRARIES_USED_LINKS.map((externalLink) => (
+                <a
+                  key={externalLink.link}
+                  href={externalLink.link}
+                  target={"_blank"}
+                  rel={"noreferrer nofollow"}
+                >
+                  <span>
+                    {<externalLink.component aria-label={externalLink.name} />}
+                  </span>
+                </a>
+              ))}
+            </span>
+          </p>
         </div>
-
-        <div className={classes.rightsText}>
-          <p>©{new Date().getFullYear()} All Rights Reserved.</p>
-        </div>
-      </div>
     </div>
+    <div className={classes.rightsText}>
+      <p>©{new Date().getFullYear()} All Rights Reserved.</p>
+    </div>
+  </div>
   );
 };
