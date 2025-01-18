41e97f5ef029dfb37069a23c6b92c9e38abea84a
Goran Subic <goran.subic@ext.webedia-group.com>
2025-01-18 11:28:08 +0100

    Igor's recommendation added.

--- src/components/Recommendations/RecommList.tsx
+++ src/components/Recommendations/RecommList.tsx
@@ -1,48 +1,50 @@
-import { Recommendations }from '@/constants/Recommendations';
-import Image from 'next/image';
-import classes from './Recomm.module.css';
-import Link from 'next/link';
- 
+import { Recommendations } from "@/constants/Recommendations";
+import Image from "next/image";
+import classes from "./Recomm.module.css";
+import Link from "next/link";
+import TextWithLineBreaks from "@/utils/textWithLineBreaks";
+
 export default async function RecommendationList() {
-    return (
-        <div className="flex items-center justify-center">
-            <ol className={classes.list}>
-                {Recommendations.map((recommendation, idx) => (
-                <li key={idx}>
-                    <div className={classes.itemWrapper}>
-                        <div className={classes.imageWrapper}>
-                            <Link href={recommendation.linkedin}>
-                                <Image
-                                    src={recommendation.image}
-                                    alt={recommendation.person}
-                                    layout={"fill"}
-                                    objectFit={"cover"}
-                                />
-                            </Link>
-                        </div>
-                        <div className={classes.contentWrapper}>
-                            <div className={classes.contentHeadingWrapper}>
-                            <p>
-                                <Link href={recommendation.linkedin}>
-                                    {recommendation.person}
-                                </Link>
-                            </p>
-                            <p>{recommendation.designation}</p>
-                            <p>({recommendation.company})</p>
-                            <p className={classes.related}>{recommendation.related}</p>
-                            </div>
-                            <div className={classes.content}>
-                                {
-                                    recommendation.description.split('\n').map((paragraph, idx) => {
-                                        return <p key={idx}>{paragraph}</p>;
-                                    })
-                                }
-                            </div>
-                        </div>
-                    </div>
-                </li>
-                ))}
-            </ol>
-        </div>
-    );
+  return (
+    <div className="flex items-center justify-center">
+      <ol className={classes.list}>
+        {Recommendations.map((recommendation, idx) => (
+          <li key={idx}>
+            <div className={classes.itemWrapper}>
+              <div className={classes.imageWrapper}>
+                <Link href={recommendation.linkedin}>
+                  <Image
+                    src={recommendation.image}
+                    alt={recommendation.person}
+                    layout={"fill"}
+                    objectFit={"cover"}
+                  />
+                </Link>
+              </div>
+              <div className={classes.contentWrapper}>
+                <div className={classes.contentHeadingWrapper}>
+                  <p>
+                    <Link href={recommendation.linkedin}>
+                      {recommendation.person}
+                    </Link>
+                  </p>
+                  <p>{recommendation.designation}</p>
+                  <p>({recommendation.company})</p>
+                  <p className={classes.related}>{recommendation.related}</p>
+                </div>
+                <div className={classes.content}>
+                  {
+                    <TextWithLineBreaks
+                      text={recommendation.description}
+                      classes={classes.paragraphBlock}
+                    />
+                  }
+                </div>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
 }
--- src/constants/Recommendations.ts
+++ src/constants/Recommendations.ts
@@ -4,6 +4,24 @@ import { IRecommendation } from "@/types/recommendations";
 export const Recommendations: IRecommendation[] = [
   {
     icon: FaJoomla,
+    image: "/images/recommendations/Igor.jpeg",
+    type: "Received",
+    linkedin: "https://www.linkedin.com/in/igor-uzelac",
+    company: "Webedia",
+    person: "Igor Uzelac",
+    designation: "Project Manager at Act Digital Serbia",
+    created: "January 2024",
+    related: "January 10, 2025, Igor managed Goran directly",
+    description:
+      `
+I had the privilege of working alongside Goran at Act Digital Serbia for almost 3 years now. During this time, I was consistently impressed by his technical expertise, work ethic, and collaborative spirit.<paragraph>
+Goran has an exceptional ability to design and implement efficient, scalable, and robust backend solutions. He is proficient in PHP, Laravel, Symfony, MySQL and have demonstrated a deep understanding of web development best practices. Goran played a pivotal role in optimising database performance, implementing secure APIs and integrating third-party services.<paragraph>
+Beyond technical skills, Goran is a fantastic team player. His consistently contributed to a positive work environment by collaborating effectively with cross-functional teams, providing constructive feedback. His ability to handle tight deadlines and solve complex problems under pressure was invaluable to our team.<paragraph>
+Therefore, I am confident that Goran will be a tremendous asset to any team !
+    `,
+  },
+  {
+    icon: FaJoomla,
     image: "/images/recommendations/Olivier.jpeg",
     type: "Received",
     linkedin: "https://www.linkedin.com/in/olivierdepiesse",
@@ -14,8 +32,8 @@ export const Recommendations: IRecommendation[] = [
     related: "December 19, 2024, Olivier was senior to Goran but didn't manage Goran directly",
     description:
       `
-I've had the privilege of working with Goran for several years, and his impact on our backend development team has been tremendous. Goran played a key role in building and maintaining the backend for our high-traffic publishing platform, ensuring it stayed reliable and fast even under heavy audience loads. \n
-His technical expertise and attention to detail have been critical to our success. Beyond their skills, Goran is a team player who's always ready to tackle tough challenges and share knowledge with others. Any team would be lucky to have him! \n
+I've had the privilege of working with Goran for several years, and his impact on our backend development team has been tremendous. Goran played a key role in building and maintaining the backend for our high-traffic publishing platform, ensuring it stayed reliable and fast even under heavy audience loads.<paragraph>
+His technical expertise and attention to detail have been critical to our success. Beyond their skills, Goran is a team player who's always ready to tackle tough challenges and share knowledge with others. Any team would be lucky to have him!<paragraph>
     `,
   },
   {
@@ -30,10 +48,10 @@ His technical expertise and attention to detail have been critical to our succes
     related: "December 12, 2024, Damir worked with Goran on the same team",
     description:
       `
-I had the privilege of working with Goran Subić at Act Digital Serbia, and I can confidently say he is an exceptional professional in his field. Goran is highly skilled in designing and developing complex software solutions, with particular expertise in API design, database optimization, and implementing cutting-edge technologies. \n
-Goran's technical acumen is matched by his problem-solving mindset. He has an innate ability to break down intricate challenges into manageable solutions, which consistently leads to high-quality results. His deep understanding of software engineering principles, coupled with his hands-on experience in leading projects, makes him an invaluable team member. \n
-What truly sets Goran apart is his personality. He is approachable, kind, and an excellent team player who fosters a positive and collaborative working environment. Goran is always willing to share his knowledge, offer support, and ensure his colleagues feel empowered to succeed. His sense of humor and down-to-earth nature make him a joy to work with. \n
-Goran is also dedicated to continuous learning, staying ahead of trends, and integrating new tools and methodologies to enhance team efficiency. I wholeheartedly recommend him to any organization seeking a highly talented and reliable software engineer. His professionalism, expertise, and outstanding character make him a significant asset to any team. \n
+I had the privilege of working with Goran Subić at Act Digital Serbia, and I can confidently say he is an exceptional professional in his field. Goran is highly skilled in designing and developing complex software solutions, with particular expertise in API design, database optimization, and implementing cutting-edge technologies.<paragraph>
+Goran's technical acumen is matched by his problem-solving mindset. He has an innate ability to break down intricate challenges into manageable solutions, which consistently leads to high-quality results. His deep understanding of software engineering principles, coupled with his hands-on experience in leading projects, makes him an invaluable team member.<paragraph>
+What truly sets Goran apart is his personality. He is approachable, kind, and an excellent team player who fosters a positive and collaborative working environment. Goran is always willing to share his knowledge, offer support, and ensure his colleagues feel empowered to succeed. His sense of humor and down-to-earth nature make him a joy to work with.<paragraph>
+Goran is also dedicated to continuous learning, staying ahead of trends, and integrating new tools and methodologies to enhance team efficiency. I wholeheartedly recommend him to any organization seeking a highly talented and reliable software engineer. His professionalism, expertise, and outstanding character make him a significant asset to any team.<paragraph>
       `,
   },
   {
@@ -48,9 +66,9 @@ Goran is also dedicated to continuous learning, staying ahead of trends, and int
     related: "March 25, 2022, Peter was senior to Goran but didn't manage Goran directly",
     description:
       `
-We started working with Goran in November 2021. Before that, he did online training with the help of one of our developers as his mentor. Goran was very dedicated during the training period, he came back with the solutions for the tasks promptly. He was also paying attention to the feedback given and could update his code based on them. He took effective steps to develop his knowledge and skills. \n
-When he had to work on client projects, he could report about his tasks in a team and business setting (with clients) in written English. He was able to dig deeper into specific fields as directed. Goran delivered meaningful work as a member of the team doing the tasks assigned to him. Unfortunately, the communication slowed down with the client, and we couldn't find other projects for him to contribute to, so we had to pause our cooperation. \n
-As Goran's dedication is unquestionable, he works hard on the tasks assigned to him, and he delivers quality solutions, I think he is a great fit for the developer position. \n
+We started working with Goran in November 2021. Before that, he did online training with the help of one of our developers as his mentor. Goran was very dedicated during the training period, he came back with the solutions for the tasks promptly. He was also paying attention to the feedback given and could update his code based on them. He took effective steps to develop his knowledge and skills.<paragraph>
+When he had to work on client projects, he could report about his tasks in a team and business setting (with clients) in written English. He was able to dig deeper into specific fields as directed. Goran delivered meaningful work as a member of the team doing the tasks assigned to him. Unfortunately, the communication slowed down with the client, and we couldn't find other projects for him to contribute to, so we had to pause our cooperation.<paragraph>
+As Goran's dedication is unquestionable, he works hard on the tasks assigned to him, and he delivers quality solutions, I think he is a great fit for the developer position.<paragraph>
       `,
   },
 ];
